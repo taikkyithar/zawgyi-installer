cfece5558a7c7fed408faef5a49ccb1d01bae0e7
taikkyithar <bloggertaikkyithar@gmail.com>
2018-04-27 14:13:42 +0630

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ clear
 echo "Zawgyi Installer"
 echo "Run with sudo or root user"
 echo "Need internet Connection"
-wget  taikkyithar.github.io/zawgyi_keyboard-0.3.3.tar.gz 
+wget  https://github.com/taikkyithar/zawgyi-installer/raw/master/zawgyi_keyboard-0.3.3.tar.gz
 tar -xvf zawgyi_keyboard-0.3.3.tar.gz 
 cd zawgyi
 chmod +x zawgyi_keyboard.py
